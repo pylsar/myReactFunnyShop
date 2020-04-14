5b5ed8cf2b464456ea77c53022f2eed68c8a89a7
pylsar <kysov2006@bk.ru>
2020-04-14 07:44:02 +0300

    search feature

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {getAllProducts} from '../../API'
 import ProductItem from '../../components/ProductItem/ProductItem'
 
+import './Search.scss'
+
 class Search extends React.Component {
     state={
         search: '',
@@ -24,22 +26,24 @@ class Search extends React.Component {
     }
 
     render(){
+        const filterSearch = this.state.products.filter(product =>{
+                return (
+                    // в нижний регистр и убираем все что не совпадает
+                    product.title.toLowerCase().indexOf(this.state.search.toLowerCase()) >-1
+                    // выводим все совпадения
+                )}).map(product => {
+                return (
+                    < ProductItem key={product.id} product={product} />
+                )
+            })
         return (
-            <div>
+            <div className="search">
                 <h2>Введите имя товара</h2>
                 <input onChange={this.handleSearch} type="text" value={this.state.search} />
+                {/* выводим текст поиска на экран */}
                 {this.state.search && <p>Вы ввели: {this.state.search}</p>}
-                
-                {this.state.products.filter(product =>{
-                    return (
-                        // в нижний регистр и убираем все что не совпадает
-                        product.title.toLowerCase().indexOf(this.state.search.toLowerCase()) >-1
-                        // выводим все совпадения
-                    )}).map(product => {
-                    return (
-                        < ProductItem key={product.id} product={product} />
-                    )
-                })}
+                {/* выводим карточку товара если не пустая строка поиска */}
+                {this.state.search && <div className="search__box">{filterSearch}</div> } 
             </div> 
         )
     }
