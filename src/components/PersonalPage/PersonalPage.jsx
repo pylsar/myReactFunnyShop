50b3fcc2326513ace878f6a7745a978faaa19465
pylsar <kysov2006@bk.ru>
2020-05-15 07:39:02 +0300

    promocode

--- src/components/PersonalPage/PersonalPage.jsx
+++ src/components/PersonalPage/PersonalPage.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const PersonalPage = ({ firstname }) => {
-    return (
-        <div>
-            <p>Добро пожаловать в личный кабинет, {firstname}</p>
-        </div>
-    )
+import './PersonalPage.scss'
+
+class PersonalPage extends React.Component{
+
+    state={
+        promocode: ''
+    }
+
+    handlePromocode = () =>{
+        this.setState({
+            promocode: Math.random()
+        })
+        
+    }
+    render(){
+        return (
+            <div className="personalPage">
+                <p>Добро пожаловать в личный кабинет, {this.props.firstname}</p>
+                <button onClick={this.handlePromocode}>получить промокод</button>
+                <p>{this.state.promocode}</p>
+            </div>
+        )
+    }
 }
 
 PersonalPage.propTypes = {
