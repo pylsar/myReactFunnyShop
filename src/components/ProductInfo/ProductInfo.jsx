5353a4c6acee2aa2bee5e5e51cee587dd61a5328
pylsar <kysov2006@bk.ru>
2020-05-19 07:13:32 +0300

    styles

--- src/components/ProductInfo/ProductInfo.jsx
+++ src/components/ProductInfo/ProductInfo.jsx
@@ -12,15 +12,19 @@ const ProductInfo = ({product, handleAddToCart, productInCart, handleRemoveFromC
         <div className="productInfo">
             <div className="productInfo__left">
                 <h3>{product.title}</h3>
-                <div className="productInfo__left__img">
-                    <img src={require(`../../assets/img/${product.img}`)} alt={product.title}/>
+                <div className="productInfo__left__mobile">
+                    <div className="productInfo__left__img">
+                        <img src={require(`../../assets/img/${product.img}`)} alt={product.title}/>
+                    </div>
+                    <div className="productInfo__left__mobile__content">
+                        <span>Цена: {product.price} {product.currency}</span>
+                        {/* если товар в корзине то убавляем кол-во */}
+                        { productInCart ? <span>Кол-во: {product.quantity -1}</span> : <span>Кол-во: {product.quantity}</span> }
+                        <div className="productInfo__left__descr">{product.longDescr}</div>
+                        <Link to='/products' className="productInfo__left--link">назад</Link>
+                        { productInCart ? <button className="productInfo__left__btn" onClick={handleRemoveFromCart}>Убрать из карзины</button> : <button className="productInfo__left__btn" onClick={handleAddToCart}>Добавить в корзину</button> }
+                    </div>
                 </div>
-                <span>Цена: {product.price} {product.currency}</span>
-                {/* если товар в корзине то убавляем кол-во */}
-                { productInCart ? <span>Кол-во: {product.quantity -1}</span> : <span>Кол-во: {product.quantity}</span> }
-                <div className="productInfo__left__descr">{product.longDescr}</div>
-                <Link to='/products' className="productInfo__left--link">назад</Link>
-                { productInCart ? <button className="productInfo__left__btn" onClick={handleRemoveFromCart}>Убрать из карзины</button> : <button className="productInfo__left__btn" onClick={handleAddToCart}>Добавить в корзину</button> }
             </div>
             <Comments />
         </div>
